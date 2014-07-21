8fdcbe0af2de008f344b913d85717b555a5b580d
Nicolas Sotty <nicolas.sotty@gmail.com>
2014-07-21 18:31:24 +0200

    delete debug

--- grails-app/services/org/gnk/rights/RightsService.groovy
+++ grails-app/services/org/gnk/rights/RightsService.groovy
@@ -4,9 +4,6 @@ class RightsService {
 
     boolean hasRight(int userRight, int lvlRight) {
 
-        print userRight
-        print lvlRight
-        print ((userRight & lvlRight) == 0)
         if ((userRight & lvlRight) == 0)
             false
         else
