14c1e3cc7d9547987ed9a254aed7d95da5431bae
pco <pierre.coppee@enioka.com>
2014-07-28 00:36:05 +0200

    TagRelation.myFindWhere coded

--- grails-app/domain/org/gnk/tag/TagRelation.groovy
+++ grails-app/domain/org/gnk/tag/TagRelation.groovy
@@ -20,7 +20,7 @@ class TagRelation {
         version type: 'integer'
     }
 
-    static def findParents(Tag tag)
+    static ArrayList<Tag> findParents(Tag tag)
     {
         List<Tag> resultList = new ArrayList<Tag>()
         Tag t = tag
@@ -31,32 +31,34 @@ class TagRelation {
             resultList.add(t)
             t = t.parent
         }
+
+        return resultList
     }
 
     static public TagRelation myFindWhere(Tag tag1Par, Tag tag2Par) {
 
-//        if (TagRelation.findWhere(tag1: tag1Par, tag2: tag2Par) == null)
-//        {
-//            List<Tag> refChildren = new TagService().findChildren(tag2Par)
-//            List<Tag> chalParents = findParents(tag1Par)
-//            Triplet<Tag, Tag, Integer> relation = new Triplet<Tag, Tag, Integer>(null, null, null)
-//
-//            relation.add(null, null, 0)
-//
-//            for (int i = 0; i < refChildren.size(); i++)
-//            {
-//                for (int j = 0; j < chalParents.size(); j++)
-//                {
-//                    if (TagRelation.findWhere(tag1: refChildren.get(i), tag2: chalParents.get(j)) > relation.getValue2())
-//                        relation.setAt0(refChildren.get(i))
-//                    relation.setAt1(chalParents.get(j))
-//                    relation.setAt2(Math.abs(TagRelation.findWhere(tag1: refChildren.get(i), tag2: chalParents.get(j))))
-//                }
-//            }
-//
-//            return TagRelation.findWhere(tag1: relation.value0, tag2: relation.value1)
-//        }
-//        else
+        if (TagRelation.findWhere(tag1: tag1Par, tag2: tag2Par) == null)
+        {
+            List<Tag> refChildren = new TagService().findChildren(tag2Par)
+            List<Tag> chalParents = findParents(tag1Par)
+            Triplet<Tag, Tag, Integer> relation = new Triplet<Tag, Tag, Integer>(null, null, null)
+
+            relation.add(null, null, 0)
+
+            for (int i = 0; i < refChildren.size(); i++)
+            {
+                for (int j = 0; j < chalParents.size(); j++)
+                {
+                    if (TagRelation.findWhere(tag1: refChildren.get(i), tag2: chalParents.get(j)) > relation.getValue2())
+                        relation.setAt0(refChildren.get(i))
+                    relation.setAt1(chalParents.get(j))
+                    relation.setAt2(Math.abs(TagRelation.findWhere(tag1: refChildren.get(i), tag2: chalParents.get(j))))
+                }
+            }
+
+            return TagRelation.findWhere(tag1: relation.value0, tag2: relation.value1)
+        }
+        else
             return TagRelation.findWhere(tag1: tag1Par, tag2: tag2Par)
 
     }
