c9c1e9389745b34d90f2235d36c9fc3a67c985ea
julienzup <julien.bohon@gmail.com>
2015-07-22 23:23:14 +0200

    change text

--- grails-app/controllers/org/gnk/publication/PublicationController.groovy
+++ grails-app/controllers/org/gnk/publication/PublicationController.groovy
@@ -77,7 +77,6 @@ class PublicationController {
         GnXMLWriterService gnXMLWriterService = new GnXMLWriterService()
         //lines below can be commented to go back to substitution step when leaving the GN creation during publication
         gn.step = "publication";
-        gn.isSubDate = true
         gn.dtd = gn.dtd.replace("<STEPS last_step_id=\"substitution\">", "<STEPS last_step_id=\"publication\">");
         if (!gn.save(flush: true, failOnError: true)) {
 
