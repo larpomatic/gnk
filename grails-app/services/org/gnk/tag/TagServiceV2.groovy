b938b449a1dc88cc7763ea3558c863b37f487c15
Jacky <chemou.j@gmail.com>
2016-09-29 18:58:57 +0200

    review of all function

--- grails-app/services/org/gnk/tag/TagServiceV2.groovy
+++ grails-app/services/org/gnk/tag/TagServiceV2.groovy
@@ -12,7 +12,6 @@ class TagServiceV2 {
 
     private static int IDgenericUniverTag = 33089;
 
-
     /**
      * get the universe from the gn
      * @param gn
@@ -20,18 +19,6 @@ class TagServiceV2 {
      */
     Tag getUniver(Gn gn) {
         return gn.getUnivers();
-    }
-
-    def findChildren(Tag t) {
-        def tags = Tag.findAllWhere(parent: t)
-        def tagsTmp = new ArrayList()
-        tagsTmp.addAll(tags)
-        if (tagsTmp == null)
-            return tags
-        for (Tag tag : tagsTmp) {
-            tags.addAll(findChildren(tag))
-        }
-        return tags
     }
 
     /**
@@ -42,16 +29,16 @@ class TagServiceV2 {
         ArrayList<Tag> UniverListTag = new ArrayList<Tag>();
         Tag genericUnivers = Tag.findById(IDgenericUniverTag);
 
-        UniverListTag = Tag.findAllByParent(parent: genericUnivers);
+        def parentList = Tag.findAllByParent(parent: genericUnivers);
+        UniverListTag.addAll(parentList);
         Collections.sort(UniverListTag);
 
-       //    for (Tag child in genericUnivers.children)
-       //       UniverListTag.add(child);
-       // Collections.sort(tagUniversList, new ComparateurTag())
+        //    for (Tag child in genericUnivers.children)
+        //       UniverListTag.add(child);
+        // Collections.sort(tagUniversList, new ComparateurTag())
 
         return UniverListTag;
     }
-
 
     /**
      * Calculate the total score of similitude between a GenericObjet and an Objet
@@ -63,16 +50,12 @@ class TagServiceV2 {
     Long computeComparativeScoreObject(Object genericObject, Object object, Gn gn) {
 
         Map<Tag, Integer> map_genericObject = initGenericObjectList(genericObject, gn);
-
         Map<Tag, Integer> map_Object = initObjectList(object);
-
 
         Long score = 0;
 
-
         for (Map.Entry<Tag, Integer> entry_generic : map_genericObject.entrySet()) {
-            for (Map.Entry<Tag, Integer> entry : map_Object.entrySet())
-            {
+            for (Map.Entry<Tag, Integer> entry : map_Object.entrySet()) {
                 if (entry_generic.getKey().getId().equals(entry.getKey().getId())) {
                     score += computeCumulativeScoreTags(entry_generic.getKey(), entry_generic.getValue(), entry.getValue());
                     score = tagUniversTreatment(entry_generic.getKey(), score, map_genericObject);
@@ -87,7 +70,7 @@ class TagServiceV2 {
      * Initialize the list of tags with the weight of GenericObject Place/Resource and a gn
      * @param GenericObject
      * @param gn
-     * @return Map<Tag, Integer>
+     * @return Map < Tag , Integer >
      */
     Map<Tag, Integer> initGenericObjectList(Object GenericObject, Gn gn) {
 
@@ -115,9 +98,9 @@ class TagServiceV2 {
         // chaque poids d'un tag normal d'une intrigue est pondéré à 20%
         Set<Plot> plotlist = gn.selectedPlotSet;
         for (Plot p : plotlist) {
-             for (PlotHasTag tp : p.plotHasTag) {
-                 map_tags.put(tp.tag, new Integer((int)tp.weight * 0.2));
-             }
+            for (PlotHasTag tp : p.plotHasTag) {
+                map_tags.put(tp.tag, new Integer((int) tp.weight * 0.2));
+            }
         }
 
         return map_tags;
@@ -126,7 +109,7 @@ class TagServiceV2 {
     /**
      * Initialize the tags list of an object Place/resource
      * @param object
-     * @return Map<Tag, Integer>
+     * @return Map < Tag , Integer >
      */
     Map<Tag, Integer> initObjectList(Object object) {
 
@@ -140,7 +123,7 @@ class TagServiceV2 {
     /**
      * get tags with weights from an object Place/Resource witch is generic or not
      * @param object
-     * @return Map<Tag, Integer>
+     * @return Map < Tag , Integer >
      */
     Map<Tag, Integer> getRelevantTags(Object object) {
 
@@ -188,7 +171,8 @@ class TagServiceV2 {
      * @return
      */
     Map<Tag, Integer> getParentTags(Object object) {
-        def tags = org.gnk.tag.Tag.findAllWhere(parent: object)
+        Map<Tag, Integer> tags = Tag.findByParent(parent: object);
+
         return tags;
     }
 
@@ -199,14 +183,13 @@ class TagServiceV2 {
      * @param map_genericObject
      * @return the reduced tag score
      */
-    int tagUniversTreatment(Tag tag, Long score, Map<Tag, Integer> map_genericObject){
-        int dividedNumber = map_genericObject.size() / 3;
+    Long tagUniversTreatment(Tag tag, Long score, Map<Tag, Integer> map_genericObject) {
+        Long dividedNumber = map_genericObject.size() / 3;
 
-        if (tag.name.toLowerCase().contains("Univers".toLowerCase()))
-            if (dividedNumber > 1)
-                score *= dividedNumber;
+        if ((tag.parentId == IDgenericUniverTag) && (dividedNumber > 1))
+            score *= dividedNumber;
 
-            return score;
+        return score;
     }
 
     /**
@@ -216,17 +199,15 @@ class TagServiceV2 {
      * @param Pweight
      * @return the weight
      */
-    Long computeCumulativeScoreTags(Tag tag, Integer GPweight, Integer Pweight)  {
+    Long computeCumulativeScoreTags(Tag tag, Integer GPweight, Integer Pweight) {
         long score = 0;
 
         score = Math.abs(GPweight) + Math.abs(Pweight);
-        if (GPweight*Pweight == -1)
+        if (GPweight * Pweight == -1)
             score *= -1;
 
         return score;
     }
-
-    // Retourne le poids d’un Tag père en fonction du poids de son fils et de la force de sa relation avec lui.
 
     /**
      * compute the father tag weight in function of ths son weight and the level of relationship with him.
@@ -253,7 +234,7 @@ class TagServiceV2 {
      * @param integer
      * @return The map modified or not.
      */
-    private Map<Tag, Integer> addTag (Map<Tag, Integer> map, Tag tag, Integer integer) {
+    private Map<Tag, Integer> addTag(Map<Tag, Integer> map, Tag tag, Integer integer) {
         Integer testValue = map.get(tag);
 
         if (testValue) {
