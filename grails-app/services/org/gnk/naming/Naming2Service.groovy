7c97c3b9a33a68b98ae770579a219e9bf1007287
Joseph <josephdiomande@gmail.com>
2017-10-14 14:42:01 +0200

    Restart reengineering of naming module

--- grails-app/services/org/gnk/naming/Naming2Service.groovy
+++ grails-app/services/org/gnk/naming/Naming2Service.groovy
@@ -8,34 +8,17 @@ import org.gnk.tag.V2TagService
 import org.gnk.utils.Pair
 
 class Naming2Service {
-    /** Objectif: remplacer l'appel à l'ancien NamingService dans le fichier IntegrationHandler
-     * en faisant intervenir le nouveau service V2TagService.
-     * return une Linkedlist<PersoForNaming>
-     */
+
     def serviceMethod() {}
 
     V2TagService v2TagService
-    Integer selectionNumber = 10 //10 noms proposés
+    Integer selectionNumber = 10
+    LinkedList<String> usedFirstName = new LinkedList<String>()
+    LinkedList<String> usedName = new LinkedList<String>()
 
-    //adapter pour des characters
     LinkedList<PersoForNaming> findBestObjects(LinkedList<PersoForNaming> persoList, Integer gn_id)
     {
-        // liste contenant les objets et leurs scores par rapport au GenericObject
-        ArrayList<Pair<ReferentialObject, Integer>> sorted_list = new ArrayList<>();
 
-        // on récupère la liste des names et leurs scores
-        List<ReferentialObject> all_names = //genericObject.getReferentialObject()
-        for (ReferentialObject p : all_names) {
-            sorted_list.add(new Pair<ReferentialObject, Integer>(p, new Integer((int) v2TagService.computeComparativeScoreObject(genericObject, p, gn))))
-        }
-
-        // on trie la sorted_list en fonction du poids de l'object
-        Collections.sort(sorted_list, new Comparator<Pair<ReferentialObject, Integer>>() {
-            public int compare(final Pair<ReferentialObject, Integer> o1, final Pair<ReferentialObject, Integer> o2) {
-                return o1.right.intValue().compareTo(o2.right.intValue());
-            }
-        });
-        Collections.reverse(sorted_list)
 
     }
 
